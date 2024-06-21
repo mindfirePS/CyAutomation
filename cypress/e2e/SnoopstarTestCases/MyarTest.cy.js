f0280105d2a51bd83bfeead4ddf46b4d9d3207f0
PRASENJITSWIN10\prasenjits <Prasenjits@mindfiresolutions.com>
2024-06-21 13:52:25 +0530

    Modified TC

--- cypress/e2e/SnoopstarTestCases/MyarTest.cy.js
+++ cypress/e2e/SnoopstarTestCases/MyarTest.cy.js
@@ -52,7 +52,7 @@
 
     })
         
-      it.only("Login Test with different inputs",()=>{
+      it("TC-001 Login Test with different inputs",()=>{
         loginPageObj.clickOnLangDD();
         loginPageObj.selectLang("EN");
         cy.fixture(fixtureFileName).then((data)=>{
@@ -84,7 +84,33 @@
         })
       })    
   })
+/*
+  describe("",()=>{
+    it("",()=>{
+      Cypress.on('uncaught:exception', (err, runnable) => {
+        // returning false here prevents Cypress from failing the test
+        if (err.message.includes('L is not defined')) {
+          return false
+        }
+      });
+      cy.visit(Cypress.env("url"));
+  
+      loginPageObj.clickOnLangDD();
+      loginPageObj.selectLang("EN");
+      loginPageObj.enterEmail(Cypress.env("email"));
+      loginPageObj.enterPassword(Cypress.env("password"));
+      loginPageObj.clickLogin();
+  
+      
+      dashboadPageObj.checkDashboardVisibility();
 
+      dashboadPageObj.clickUser();
+      dashboadPageObj.clickLogOut();
+  
+      loginPageObj.checkLoginButtonVisibility();
+    })
+  })
+  */
   describe('Snoopstar test cases', () => {
 
   beforeEach("Login to Snoopstar",()=>{
@@ -106,48 +132,6 @@
     dashboadPageObj.checkDashboardVisibility();
   })
   
-    it.skip('Standard Product Creation', () => {
-      
-      dashboadPageObj.clickOnStartNewProduct();
-      
-      selectProductTypePageObj.clickOnConfirmButton();
-
-      stdProdSetupPageObj.enterProductName("CyStdPrd");
-      stdProdSetupPageObj.selectProductType("Booklets");
-      stdProdSetupPageObj.selectActCountries();
-
-      actCountryPageObj.clickOnFastSelection();
-      actCountryPageObj.selectCountry("America");
-      actCountryPageObj.clickSaveAndClose();
-      
-      stdProdSetupPageObj.clickMasterLangSetup();
-
-      masterLangPageObj.selectMasterLanguage();
-      
-      actCountryPageObj.clickSaveAndClose();
-
-      cy.wait(2000);
-      stdProdSetupPageObj.clickSaveAndOpenInEditor();
-
-      editorPageObj.verifyProdName("CyStdPrd");
-
-      
-    })
-    it.skip("Delete a product",()=>{
-      cy.wait(2000);
-      dashboadPageObj.clickOnProducts();
-      productsPageObj.enterProductNameInSearch("CyStdPrd");
-      productsPageObj.verifyProductName("CyStdPrd");
-      productsPageObj.clickProdTrashIcon();
-
-      delContentPageObj.verifyProductName("CyStdPrd");
-      delContentPageObj.clickDeleteButton();
-      flashMsgObj.verifyStatus("Success");
-      flashMsgObj.verifyMessage("Product deleted successfully.");
-      productsPageObj.verifyNoDataAvailableIsDisplayed();
-      productsPageObj.closeProducts();
-    })
-   
     it.skip("Open a Product",()=>{
       cy.wait(2000);
       dashboadPageObj.clickOnProducts();
@@ -159,41 +143,8 @@
 
     })
 
-    it.skip("Add target",()=>{
-      cy.wait(2000);
-      dashboadPageObj.clickOnProducts();
-      productsPageObj.enterProductNameInSearch("CyStdPrd");
-      productsPageObj.verifyProductName("CyStdPrd");
-      productsPageObj.clickShowLanguages();
-      productsPageObj.clickOpenProdInEditor();
-      editorPageObj.verifyProdName("CyStdPrd");
-
-      editorPageObj.clickAddTarget();
-      targetPgObj.clickOnSelect();
-      mediaPgObj.enterMediaToSearch("panda.jpg");
-      mediaPgObj.clickOnMediaName();
-      mediaPgObj.clickOnSelect();
-
-      cy.wait(2000);
-      targetImageSizePgObj.clickOnOk();
-
-      flashMsgObj.verifyStatus("Processing");
-      flashMsgObj.verifyMessage("Uploading, please wait ...");
-
-      targetPgObj.clickOnSaveChanges()
-
-      flashMsgObj.verifyStatus("Processing");
-      flashMsgObj.verifyMessage("Given image will be processed in the background.");
-      
-      cy.wait(4500);
-      flashMsgObj.verifyStatus("Success");
-      flashMsgObj.verifyMessage("Target sync finished");
-      //Processing
-      //Given image will be processed in the background.
-      //Success
-      //Target sync finished
-    })
-    it.skip("Creating a Standard Product and deleting it",()=>{
+   
+    it("Creating a Standard Product and deleting it",()=>{
       dashboadPageObj.clickOnStartNewProduct();
       
       selectProductTypePageObj.clickOnConfirmButton();
@@ -252,7 +203,8 @@
       productsPageObj.verifyNoDataAvailableIsDisplayed();
       productsPageObj.closeProducts();
     })
-    it.skip("TC-112 Snoopstar-1.8.4 MYAR-1935 Recreating snoopcodes leads to an error : Verify personlizing shortcode is possible after deactivating and reactivating Experience sharing",()=>{
+
+    it("TC-112 Snoopstar-1.8.4 MYAR-1935 Recreating snoopcodes leads to an error : Verify personlizing shortcode is possible after deactivating and reactivating Experience sharing",()=>{
       cy.wait(2000);
       dashboadPageObj.clickOnProducts();
       productsPageObj.enterProductNameInSearch("TestMyar1935Std");
@@ -287,12 +239,12 @@
         ExpSharLinkSnoopCodeObj.clickExpSharLinkSnoopCodeModalClose();
 
     })
-    /*
+    // /*
     afterEach("Logout from snoopstar",()=>{
       dashboadPageObj.clickUser();
       dashboadPageObj.clickLogOut();
   
       loginPageObj.checkLoginButtonVisibility();
     })
-    */
+    // */
   })
